4953d766c6dd12da52b8883816d26781a2bd9e49
Mark Bonicillo <mark.bonicillo@pnnl.gov>
2021-07-13 15:13:47 -0700

    Update action

--- run-pytest.sh
+++
@@ -1,6 +0,0 @@
-source env/bin/activate
-
-pip install -e .
-
-pytest volttrontesting/testutils -rf -o junit_family=xunit2
---junitxml=output/test-testutils-${{ inputs.os }}-${{ inputs.python_version }}-results.xml
